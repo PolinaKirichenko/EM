b96c5d63cfcc74201dfa93a7acafff73496de05a
polkirichenko <pol.kirichenko@mail.ru>
2016-03-23 01:36:56 +0300

    -list comprehensions

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from math import *
 from scipy.stats import multivariate_normal, rv_discrete
 import matplotlib.pyplot as plt
 import matplotlib.mlab as mlab
+import time
 
-class Gaus:
+class Gauss:
     def __init__(self, m, c):
         self.dim = m.size
         self.mu = m
@@ -15,14 +16,13 @@ class Gaus:
 
 
 class GMM:
-    def Estep(self, train_set, gamma):
+    def Estep(self, train_set, norm, gamma):
         tss = train_set.shape[0]
-        norm = [[self.gaussians[i].density(train_set[j]) for j in range(tss)] for i in range(self.compnum)]
         prob = np.dot(self.w, norm)  # prob[j] probability of observation j
 
         for i in range(self.compnum):
-            for j in range(tss):
-                gamma[i][j] = self.w[i] * self.gaussians[i].density(train_set[j]) / prob[j]
+             for j in range(tss):
+                 gamma[i][j] = self.w[i] * norm[i][j] / prob[j]
 
     def Mstep(self, train_set, gamma):
         tss = train_set.shape[0]
@@ -37,10 +37,15 @@ class GMM:
                 self.gaussians[i].cov += gamma[i][n] * np.asmatrix(centre[n]).T * centre[n]
             self.gaussians[i].cov /= exnum[i]
 
-    def logLikelihood(self, train_set):
+    def updateProbabilities(self, norm, train_set):
+        tss = train_set.shape[0]
+        for i in range(self.compnum):
+            for j in range(tss):
+                norm[i][j] = self.gaussians[i].density(train_set[j])
+
+    def logLikelihood(self, train_set, norm):
         tss = train_set.shape[0]
         ll = 0
-        norm = [[self.gaussians[i].density(train_set[j]) for j in range(tss)] for i in range(self.compnum)]
         prob = np.dot(self.w, norm)
         for i in range(tss):
             ll += log(prob[i])
@@ -51,19 +56,28 @@ class GMM:
         self.dim = mu[0].size # dimension
         self.compnum = w.size # the number of gaussians
         self.w = w # weights of gaussians
-        self.gaussians = [Gaus(mu[i], cov[i]) for i in range(self.compnum)]
+        self.gaussians = [Gauss(mu[i], cov[i]) for i in range(self.compnum)]
 
     def printParam(self):
+        print(self.w)
         for g in self.gaussians:
             print(g.mu)
-            print(g.cov)
-            print()
+            print(g.cov, '\n')
 
-    def EM(self, train_set):
-        gamma = np.zeros((self.compnum, train_set.shape[0]))
-        self.Estep(train_set, gamma)
-        self.Mstep(train_set, gamma)
-        print(self.logLikelihood(train_set))
+    def EM(self, train_set, iter):
+        tss = train_set.shape[0]
+        gamma = np.zeros((self.compnum, tss))
+        norm = np.zeros((self.compnum, tss))
+        self.updateProbabilities(norm, train_set)
+
+        for i in range(iter):
+            begin = time.clock()
+            self.Estep(train_set, norm, gamma)
+            self.Mstep(train_set, gamma)
+            self.updateProbabilities(norm, train_set)
+            print(self.logLikelihood(train_set, norm))
+            end = time.clock()
+            print(end - begin, '\n')
 
 
 def initParam(gnum, obs):
@@ -71,23 +85,33 @@ def initParam(gnum, obs):
     w = np.random.random(gnum)
     w /= w.sum()
 
-    mu = np.dot(np.random.random_sample((gnum, dim)), np.diag(np.amax(obs, axis=0)) - np.amin(obs, axis=0)) + \
+    mu = np.random.random_sample((gnum, dim)) * (np.amax(obs, axis=0) - np.amin(obs, axis=0)) + \
          np.full((gnum, dim), np.amin(obs, axis=0))
     
-    cov = np.zeros((gnum, dim, dim))
-    for i in range(gnum):
-        cov[i] = np.eye(dim, dtype = float)
+    cov = np.full((gnum, dim, dim), np.eye(dim, dtype = float))
     return w, mu, cov
-
 
 def generateSamples(w, mu, cov, s):
     dim = len(mu[0])
     d = rv_discrete(values = (range(len(w)), w))
     components = d.rvs(size=s)
+    # generate samples of size of each component, then shuffle
     if dim > 1:
         return components, np.array([np.random.multivariate_normal(mu[i], cov[i], 1)[0] for i in components])
     else:
         return components, np.asmatrix([np.random.normal(mu[i], cov[i], 1)[0] for i in components]).T
+
+def generate(true_w, true_mu, true_cov, n):
+    comps, obs = generateSamples(true_w, true_mu, true_cov, n)
+    np.savetxt("true", np.concatenate((np.asmatrix(comps).T, obs), axis=1), fmt=['%d'] + ['%f'] * obs.shape[1], delimiter='\t')
+    np.savetxt("input", obs, delimiter='\t', fmt='%f')
+    return obs
+
+def readSamples():
+    with open("input") as f:
+        obs = np.loadtxt(f)
+    return obs
+
 
 def main():
     gnum = 3
@@ -106,25 +130,19 @@ def main():
                [ [2, 1],
                  [1, 2] ]]
 
-    comps, obs = generateSamples(true_w, true_mu, true_cov, 1000)
-    np.savetxt("true", np.concatenate((np.asmatrix(comps).T, obs), axis=1), fmt=['%d'] + ['%f'] * obs.shape[1], delimiter='\t')
-    np.savetxt("input", obs, delimiter='\t', fmt='%f')
+    obs = generate(true_w, true_mu, true_cov, 1000)
+    #obs = readSamples()
+
     plt.plot(*zip(*obs), marker='o', ls='')
         
     w, mu, cov = initParam(gnum, obs)
 
     model = GMM(w, mu, cov)
-    for i in range(20):
-        model.EM(obs)
-    print(model.w)
-    print()
-    for g in model.gaussians:
-        print(g.mu)
-        print(g.cov)
-        print()
+    model.EM(obs, 15)
+    model.printParam()
 
     # Here I assume that dimension is 2 #
-    delta = 0.1
+    delta = 0.2
     obsmax = np.amax(obs, axis=0)
     obsmin = np.amin(obs, axis=0)
     x = np.arange(obsmin[0], obsmax[0], delta)
@@ -133,11 +151,8 @@ def main():
     Z = 0
     for i, g in enumerate(model.gaussians):
         Z = model.w[i] * mlab.bivariate_normal(X, Y, sigmax=g.cov[0][0], sigmay=g.cov[1][1], mux=g.mu[0], muy=g.mu[1], sigmaxy=g.cov[0][1])
-        try:
-            plt.contour(X, Y, Z)
-        except RuntimeWarning:
+        plt.contour(X, Y, Z)
             
     plt.savefig('gaus.png')
-    plt.show()
 
 main()
